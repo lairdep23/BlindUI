04f68bcf366a3dd9e33ab1519f3dcdb333c1d401
Evan <Evan@Evans-MacBook-Pro-2.local>
2016-03-31 19:13:57 -0500

    location Manager update

--- Blinders/MapVC.swift
+++ Blinders/MapVC.swift
@@ -9,21 +9,78 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var map: MKMapView!
     
+    let regionRadius: CLLocationDistance = 3000.0
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        map.delegate = self
+        
+        getPlacemarkFromAddress(selectedDate["address"]!)
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        locationAuthStatus()
     }
     
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2, regionRadius * 2)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            centerMapOnLocation(loc)
+        }
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation.isKindOfClass(DatesAnnotation) {
+            
+            let annoView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "default")
+            annoView.pinTintColor = UIColor.darkGrayColor()
+            annoView.animatesDrop = true
+            return annoView
+            
+        } else if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+        
+        return nil
+    }
+    
+    func createAnnotationForLocation(location: CLLocation) {
+        
+        let perfectDate = DatesAnnotation(coordinate: location.coordinate)
+        map.addAnnotation(perfectDate)
+    }
+    
+    func getPlacemarkFromAddress(address: String) {
+        CLGeocoder().geocodeAddressString(address) { (placemarks: [CLPlacemark]?, error: NSError?) -> Void in
+            if let marks = placemarks where marks.count > 0 {
+                if let loc = marks[0].location {
+                    self.createAnnotationForLocation(loc)
+                }
+            }
+        }
+    }
 
 
 }
